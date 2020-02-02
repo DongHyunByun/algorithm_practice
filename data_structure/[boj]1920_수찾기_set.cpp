7f55ae1c41f2443838d925a0a4bd1eab096cced8
Byun Dong Hyun <50386280+DongHyunByun@users.noreply.github.com>
2020-02-02 14:39:59 +0900

    Add files via upload

---
+++ data_structure/[boj]1920_수찾기_set.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+int N, M, temp;
+set<int> s;
+
+int main() 
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N;
+	for (int i = 0; i < N; ++i) {
+		cin >> temp;
+		s.insert(temp);
+	}
+	cin >> M;
+	for (int i = 0; i < M; ++i) {
+		cin >> temp;
+		cout << (s.find(temp) == s.end() ? 0 : 1) << '\n';
+	}
+	return 0;
+}
